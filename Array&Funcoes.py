c27933825b259407f962599b342f5b5425b9da90
Camyla Genelice <genelicecamyla@gmail.com>
2024-01-11 19:12:01 -0300

    update

--- Array&Funcoes.py
+++ Array&Funcoes.py
@@ -1,7 +1,7 @@
 # Soma de vetores
-"""
-import numpy as np
 
+import numpy as np
+"""
 v = np.array([1,2,3])
 u = np.array([2,6,3]) #negativo
 vu = np.array([])
@@ -21,3 +21,13 @@ a = np.array([4,3,1])
 b = np.array([8,2,1])
 soma_vetores(a,b)
 """
+
+vetor1 = np.array([[1,5,4],[9,3,8]])
+vetor2 = np.array([[6,1,5],[7,3,9]])
+
+for i in vetor1:
+    for a in vetor2:
+        multi = i * a + 
+        resultado = np.array([[]])
+        resultado = np.append(resultado,multi)
+    print(multi)
